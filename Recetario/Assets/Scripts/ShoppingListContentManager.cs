0b486ea6922705437e4b0ff9452beb2ec3b6be4a
Simón Ramírez <42391624+SimonRamirez155@users.noreply.github.com>
2020-10-27 18:00:14 -0600

    ver 0.28 Script para shopping list agregado.

---
+++ Recetario/Assets/Scripts/ShoppingListContentManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShoppingListContentManager : MonoBehaviour
+{
+    public Transform contentObj;
+    public GameObject pfb_grp_ingredient;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        foreach (Transform child in contentObj.transform)
+        {
+            Destroy(child.gameObject);
+        }        
+
+       for (int i = 0; i < 5; i++)
+       {
+       GameObject obj = Instantiate(pfb_grp_ingredient, contentObj.transform);
+       obj.transform.Find("btn_remove").GetComponent<Button>().onClick.AddListener(delegate {Destroy(obj);});
+       }
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
