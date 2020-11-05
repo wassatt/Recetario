c25c6a5e1f0aaf54e279419b81018b51b4f3d8a0
Fede <fjimenez.x1@gmail.com>
2020-11-05 16:30:48 -0600

    ver 0.38 Signin, Login, Logout Done

--- Recetario/Assets/Scripts/UiManagerSignin.cs
+++ Recetario/Assets/Scripts/UiManagerSignin.cs
@@ -1,42 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Linq;
 
 public class UiManagerSignin : MonoBehaviour
 {
     public InputField userMail;
     public InputField userPassword;
     public InputField userName;
-    public ToggleGroup toggleGroup;
     public ScriptableString signinMail;
     public ScriptableString signinPass;
     public ScriptableString signinName;
-    public ScriptableBool isRestaurant;
     public Button registerButton;
 
     void Start()
     {
         userMail.onValueChanged.AddListener(delegate { CheckValidInput(); });
         userPassword.onValueChanged.AddListener(delegate { CheckValidInput(); });
-        CheckIsRestaurantValue();
-    }
-
-    void OnEnable()
-    {
-        CheckIsRestaurantValue();
-    }
-
-    public void CheckIsRestaurantValue()
-    {
-        Toggle selectedToggle = toggleGroup.ActiveToggles().FirstOrDefault();
-        if (selectedToggle != null)
-        {
-            //Debug.Log(selectedToggle, selectedToggle);
-            if (selectedToggle.name == "tgl_Restaurant")
-                isRestaurant.var = true;
-            else
-                isRestaurant.var = false;
-        }
     }
 
     public void CheckValidInput()
