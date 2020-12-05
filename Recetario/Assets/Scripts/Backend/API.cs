5978464c2434994998b3a49af1fcd4d81cdc2787
Fede <fjimenez.x1@gmail.com>
2020-12-04 19:20:00 -0600

    api links

--- Recetario/Assets/Scripts/Backend/API.cs
+++ Recetario/Assets/Scripts/Backend/API.cs
@@ -1,7 +1,7 @@
 ﻿public static class API
 {
     private const string urlApi = "https://us-central1-pocketmenu-385db.cloudfunctions.net/api";
-    //private const string urlApi = "http://localhost:5000/pocketmenu-385db/us-central1/api";     
+    //private const string urlApi = "http://localhost:5000/recetario-79536/us-central1/api";     
 
     public const string urlGetUser = urlApi + "/user";
     public const string urlPostNewUser = urlApi + "/newUser";
