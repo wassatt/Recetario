7d9b635aa2643d3daa5aa149c35738a40db460a7
Fede <fjimenez.x1@gmail.com>
2021-02-09 18:37:36 -0600

    load recipes results

--- Recetario/Assets/Scripts/UiManagers/RecipesContentManager.cs
+++ Recetario/Assets/Scripts/UiManagers/RecipesContentManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using SimpleJSON;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
 public class RecipesContentManager : MonoBehaviour
 {
@@ -15,24 +16,34 @@ public class RecipesContentManager : MonoBehaviour
     private GameObject pfb_grp_recipe_preview;
     [SerializeField]
     private UiManagerFullRecipe fullRecipe;
+    [SerializeField]
+    private ScrollRect srRecipes;
 
     public UnityEvent onOpenFullRecipe;
     public UnityEvent onSearchDone;
-
+    public List<Recipe> recipes;
+    int recipesAt;
+    int instancesOffset;
     // Start is called before the first frame update
     void Start()
     {
+        recipes = new List<Recipe>();
+        instancesOffset = 4;
+        srRecipes.onValueChanged.AddListener(delegate {CheckIfScrollIsAtBottom();});
         GetRecipes();
     }
 
     public void GetRecipesBySearch()
     {
+        recipesAt = 0;
+
         foreach (Transform child in contentRecipesObj.transform)
         {
             Destroy(child.gameObject);
         }
 
         Recipe recipe = new Recipe();
+        recipes.Clear();
 
         StartCoroutine(dbManager.endpointsTools.GetWithParam(API.urlGetRecipesSearch, ifSearch.text, "", returnValue =>
         {
@@ -68,20 +79,25 @@ public class RecipesContentManager : MonoBehaviour
                     //Debug.Log(itemString);
                 }
 
-                InstantiatRecipePreview(recipe);
+                recipes.Add(recipe);
+                //InstantiateRecipePreview(recipe);
             }
+            InstantiateRecipes(recipesAt);
         }));
     }
 
     public void GetRecipes()
     {
+        recipesAt = 0;
+
         foreach (Transform child in contentRecipesObj.transform)
         {
             Destroy(child.gameObject);
         }
 
         Recipe recipe = new Recipe();
-        
+        recipes.Clear();
+
         StartCoroutine(dbManager.endpointsTools.GetWithParam(API.urlGetRecipes, "", "", returnValue =>
         {
             //Debug.Log(returnValue);
@@ -116,19 +132,35 @@ public class RecipesContentManager : MonoBehaviour
                     //Debug.Log(itemString);
                 }
 
-                InstantiatRecipePreview(recipe);
+                recipes.Add(recipe);
+                //InstantiateRecipePreview(recipe);
             }
+
+            //TODO: Shuffle
+            InstantiateRecipes(recipesAt);
         }));
+    }
+
+    public void CheckIfScrollIsAtBottom()
+    {
+        if (srRecipes.verticalNormalizedPosition <= 0.05f)
+        {
+            // get last index
+            InstantiateRecipes(recipesAt);
+        }
     }
 
     public void GetRecipesByCategory(int category)
     {
+        recipesAt = 0;
+
         foreach (Transform child in contentRecipesObj.transform)
         {
             Destroy(child.gameObject);
         }
 
         Recipe recipe = new Recipe();
+        recipes.Clear();
 
         StartCoroutine(dbManager.endpointsTools.GetWithParam(API.urlGetRecipes, $"{category}", "", returnValue =>
         {
@@ -165,12 +197,40 @@ public class RecipesContentManager : MonoBehaviour
                     //Debug.Log(itemString);
                 }
 
-                InstantiatRecipePreview(recipe);
+                recipes.Add(recipe);
+                //InstantiateRecipePreview(recipe);
             }
+                InstantiateRecipes(recipesAt);
         }));
     }
 
-    private void InstantiatRecipePreview(Recipe recipe)
+    private void InstantiateRecipes(int startAt)
+    {
+        if(recipes.Count != 0)
+        {
+            if (recipes.Count > instancesOffset)
+            {
+                for (int i = startAt; i < startAt+ instancesOffset; i++)
+                {
+                    if (i < recipes.Count)
+                        InstantiateRecipePreview(recipes[i]);
+                }
+                
+                recipesAt = startAt + instancesOffset;
+            }
+            else
+            {
+                foreach (Recipe recipe in recipes)
+                {
+                    if (recipesAt < recipes.Count)
+                        InstantiateRecipePreview(recipe);
+                    recipesAt++;
+                }
+            }
+        }
+    }
+
+    private void InstantiateRecipePreview(Recipe recipe)
     {
         GameObject obj = Instantiate(pfb_grp_recipe_preview, contentRecipesObj.transform);
         RecipeData recipeData = obj.GetComponent<RecipeData>();
@@ -188,4 +248,23 @@ public class RecipesContentManager : MonoBehaviour
             onOpenFullRecipe.Invoke();
         });
     }
+
 }
+
+//static class MyExtensions
+//{
+//    private static Random rng = new Random();
+
+//    public static void Shuffle<T>(this IList<T> list)
+//    {
+//        int n = list.Count;
+//        while (n > 1)
+//        {
+//            n--;
+//            int k = rng.Next(n + 1);
+//            T value = list[k];
+//            list[k] = list[n];
+//            list[n] = value;
+//        }
+//    }
+//}
